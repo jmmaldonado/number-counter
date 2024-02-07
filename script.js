894c7f5b1a553612f1e055a997e6f58395f61d95
Maldini <jmmaldonado@gmail.com>
2024-02-07 16:58:49 +0100

    Add buttons and new layout

--- script.js
+++ script.js
@@ -10,35 +10,10 @@ function startGame() {
     updateFontSize()
 }
 
-function handleClickOrTap(event) {
-    if (event.type === 'click') { // Handle regular clicks
-        const windowWidth = window.innerWidth;
-        const clickX = event.clientX;
-
-        if (clickX > windowWidth / 2) {
-            count++;
-        } else {
-            count = Math.max(10, count - 1);
-        }
-
-        counterDisplay.textContent = count;
-
-    } else if (event.type === 'touchend') { // Handle touch events
-        if (event.touches && event.touches[0]) { // Add this check
-
-            const windowWidth = window.innerWidth;
-            const touchX = event.touches[0].clientX;
-
-            if (touchX > windowWidth / 2) {
-                count++;
-            } else {
-                count = Math.max(10, count - 1);
-            }
-
-            counterDisplay.textContent = count;
-        }
-    }
-    updateFontSize()
+function handleClickOrTap(increment) {
+    count += increment;
+    counterDisplay.textContent = count;
+    updateFontSize();
     changeCounterColor();
 }
 
@@ -52,20 +27,20 @@ function handleOrientationChange() {
 function updateFontSize() {
     const counter = document.getElementById('counter');
     const numDigits = count.toString().length;
-    const desiredWidthPercentage = 95; // Percentage of screen width the number should occupy
+    const desiredWidthPercentage = 100; // Percentage of screen width the number should occupy
 
     // Calculate font size in pixels
-    const fontSizePercentage = desiredWidthPercentage / numDigits; // Adjust font size percentage  
-    const calculatedFontSize = Math.ceil((fontSizePercentage / 100) * window.innerWidth);
+    const fontSizePercentage = desiredWidthPercentage; // Adjust font size percentage  
+    const calculatedFontSize = Math.ceil((fontSizePercentage / 100) * window.innerWidth / numDigits);
 
     counter.style.fontSize = calculatedFontSize + 'px';
 }
 
-function hashCode(str) { 
+function hashCode(str) {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
-      hash = (str.charCodeAt(i) * 31) ^ hash; 
-      hash |= 0; // Convert to 32bit integer
+        hash = (str.charCodeAt(i) * 31) ^ hash;
+        hash |= 0; // Convert to 32bit integer
     }
     return Math.abs(hash); // Keep result positive 
 }
@@ -77,12 +52,10 @@ function colorFromHash(hash) {
 
 
 function changeCounterColor() {
-  const color = colorFromHash(hashCode(count.toString()));
-  counterDisplay.style.color = color; 
+    const color = colorFromHash(hashCode(count.toString()));
+    counterDisplay.style.color = color;
 }
 
-document.addEventListener('click', handleClickOrTap);
-document.addEventListener('touchend', handleClickOrTap);
 window.addEventListener('orientationchange', handleOrientationChange);
 
 startGame()
@@ -98,10 +71,17 @@ counterDisplay.addEventListener('click', (event) => {
 
 const languageSelect = document.getElementById('language-select');
 
-languageSelect.addEventListener('change', () => {
+languageSelect.addEventListener('change', (event) => {
     event.stopPropagation(); // Prevent clicks on flags from triggering the document listener
     const selectedLanguage = languageSelect.value;
     const utterance = new SpeechSynthesisUtterance(count.toString());
     utterance.lang = selectedLanguage;
     speechSynthesis.speak(utterance);
+});
+
+const controlButtons = document.querySelectorAll('.control-button');
+
+controlButtons.forEach(button => {
+    button.addEventListener('click', () => { handleClickOrTap(parseInt(button.dataset.value)) });
+    button.addEventListener('touchend', () => { handleClickOrTap(parseInt(button.dataset.value)) });
 });
